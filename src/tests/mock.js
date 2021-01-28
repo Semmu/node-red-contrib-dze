d19d6112fcdaee9e038fb026833c29b046a8b151
Semmu <iamsemmu@gmail.com>
2021-01-28 03:23:34 +0100

    add mocking "library"

---
+++ src/tests/mock.js
@@ -0,0 +1,62 @@
+// simple mock interface for the shared flow context in Node-RED.
+function Flow() {
+    this.data = {};
+
+    this.set = (id, value) => {
+        this.data[id] = value;    
+    }
+
+    this.get = (id) => {
+        if (id in this.data) {
+            return this.data[id];
+        } else {
+            return undefined;
+        }
+    }
+
+    // probably unnecessary.
+    this.__reset = () => {
+        this.data = {};
+    }
+
+    return this;
+}
+
+// simple mock interface for the used `node` methods in Node-RED.
+function Node() {
+    this.warns = [];
+    this.warn = (msg) => {
+        this.warns.push(msg);
+    };
+
+    this.sends = [];
+    this.send = (msg) => {
+        this.sends.push(msg);
+    };
+
+    this.isDone =  false;
+    this.done = () => {
+        this.isDone = true;
+    };
+
+    // probably unnecessary.
+    this.__reset = () => {
+        this.warns = [];
+        this.sends = [];
+        this.isDone = false;
+    };
+
+    return this;
+};
+
+// simple helper function to construct empty messages.
+function EmptyMsg() {
+    // this is supposed to be a safe cloning mechanism,
+    // not sure if needed or not.
+    return Object.assign({
+        topic: 'zigbee2mqtt/SomeDevice',
+        payload: {}
+    }, {});
+}
+
+export { Flow, Node, EmptyMsg };
