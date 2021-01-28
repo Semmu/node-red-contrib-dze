932147399de621742324ad943559bc9064888329
Semmu <iamsemmu@gmail.com>
2021-01-28 03:20:42 +0100

    add package.json + index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("There is nothing to run outside of Node-RED, only tests.");
+console.log("Run them with `npm test`.");
+console.log("For installation manuals, please check the GitHub project page:");
+console.log("https://github.com/Semmu/node-red-contrib-dze/");
