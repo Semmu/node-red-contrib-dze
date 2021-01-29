1fbe44b0e158a04146fed466eb08ce936546e808
Semmu <iamsemmu@gmail.com>
2021-01-29 22:49:05 +0100

    add testcases about Date mocking

--- src/tests/test_cases.js
+++ src/tests/test_cases.js
@@ -1,4 +1,4 @@
-import { Flow, Node, EmptyMsg } from './mock.js';
+import { Flow, Node, EmptyMsg, AlwaysTheSameDate } from './mock.js';
 import { test } from './testing_lib.js';
 
 test({
@@ -500,6 +500,27 @@ test({
     ]
 });
 
+test({
+    name: "the date testing file takes a little time to run",
+    context: () => ({}),
+    run: 'src/tests/testing_date.js',
+    asserts: [
+        'calledCount > 0',
+        'timeSpent > 0'
+    ]
+});
+
+test({
+    name: "the date testing file takes a little time to run, but mocking also works",
+    context: () => ({
+        Date: AlwaysTheSameDate
+    }),
+    run: 'src/tests/testing_date.js',
+    asserts: [
+        'calledCount > 0',
+        'timeSpent == 0'
+    ]
+});
 
 // print verdict, special case of this function
 test("verdict");
